ef078fb2844a4eb30a24e2fb5da858ab7def657a
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-01-14 15:22:44 +0530

    Time: 0 ms (100%), Space: 20.6 MB (9.15%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -6,11 +6,9 @@ class Solution:
         i =0
         for j in range(len(nums)):
                if nums[j]!=0:
-                    nums[i]=nums[j]
+                    nums[i],nums[j]=nums[j],nums[i]
                     i+=1
-        while i < len(nums):
-            nums[i] = 0
-            i+=1
+        
                 
         
 
