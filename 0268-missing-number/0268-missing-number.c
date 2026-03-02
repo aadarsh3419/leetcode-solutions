876a57e3a6ee5a3ebbe5ffdcfd6d3cbe683513a7
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-03-02 18:23:23 +0530

    Time: 0 ms (100%), Space: 9.7 MB (6.07%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.c
@@ -0,0 +1,11 @@
+int missingNumber(int* nums, int numsSize) {
+    int xor = 0;
+    for(int i = 0; i<=numsSize;i++){
+        xor ^=i;
+    }
+    for(int i =0;i< numsSize;i++){
+        xor^=nums[i];
+    }
+    return xor;
+    
+}
