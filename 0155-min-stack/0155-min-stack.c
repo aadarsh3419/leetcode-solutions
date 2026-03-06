c5969417b9650f4a5750923f4674e467f05e1ca9
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-03-06 22:57:57 +0530

    Time: 6 ms (34.39%), Space: 19.1 MB (74.48%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.c
@@ -0,0 +1,74 @@
+
+
+
+typedef struct {
+    int size;
+    int top;
+    int *arr;
+    
+} MinStack;
+
+
+MinStack* minStackCreate() {
+    MinStack* s = malloc(sizeof(MinStack));
+    s->top = -1;
+    s->size = 1000; 
+    s->arr = (int*)malloc (s->size * sizeof(int));
+
+    return s;
+}
+
+void minStackPush(MinStack* obj, int val) {
+    if(obj->top == obj->size-1)
+       return;
+    obj->top++;
+    obj->arr[obj->top] = val; 
+    
+    
+} 
+
+void minStackPop(MinStack* obj) {
+    if(obj->top == -1)
+       return;
+    obj->top--;
+    
+}
+
+int minStackTop(MinStack* obj) {
+    if(obj->top == -1)
+       return-1;
+    return obj->arr[obj->top];
+    
+}
+
+int minStackGetMin(MinStack* obj) {
+    if(obj->top == -1)
+        return-1;
+    int mini = obj->arr[0];
+    for(int i=0;i<=obj->top;i++){
+        if(obj->arr[i] < mini)
+            mini=obj->arr[i];
+    }
+    return mini;
+    
+}
+
+void minStackFree(MinStack* obj) {
+    
+    free(obj->arr);
+    free(obj);
+}
+
+/**
+ * Your MinStack struct will be instantiated and called as such:
+ * MinStack* obj = minStackCreate();
+ * minStackPush(obj, val);
+ 
+ * minStackPop(obj);
+ 
+ * int param_3 = minStackTop(obj);
+ 
+ * int param_4 = minStackGetMin(obj);
+ 
+ * minStackFree(obj);
+*/
