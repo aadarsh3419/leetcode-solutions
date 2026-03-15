6fedc63579af2f3735b54de920cffe7cf8d543fc
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-03-15 08:30:43 +0530

    Time: 0 ms (100%), Space: 9 MB (25.49%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.c
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.c
@@ -6,12 +6,12 @@
  * };
  */
 struct ListNode* middleNode(struct ListNode* head) {
-    struct ListNode *slow,*fast;
-    slow = head;
+    struct ListNode *slow, *fast;
+    slow =head;
     fast = head;
-    while(fast!=NULL&&fast->next!=NULL){
-        slow = slow->next;
+    while(fast != NULL&&fast->next!=NULL ){
+        slow = slow->next; 
         fast = fast->next->next;
-    } 
+    }
     return slow;
 }  
