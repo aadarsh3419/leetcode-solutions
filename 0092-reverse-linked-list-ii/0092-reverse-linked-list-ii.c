ac12661b68ffecae8f14f9e475d9370b87890f9c
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-03-15 21:45:15 +0530

    Time: 0 ms (100%), Space: 9.6 MB (10.79%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.c
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.c
@@ -12,17 +12,23 @@ struct ListNode* reverseBetween(struct ListNode* head, int left, int right) {
     struct ListNode  dummy;
     dummy.next = head;
     
+    
     struct ListNode* prev = &dummy;
-    for(int i=1; i < left ;i++){
+
+    for(int i = 1; i < left;i++){
         prev = prev->next;
     }
+
     struct ListNode* current = prev->next;
-    
-    for(int i=0 ; i < right-left; i++){
+
+    for(int i=0;i<right-left;i++){
         struct ListNode* temp = current->next;
         current->next = temp->next;
         temp->next = prev->next;
         prev->next = temp;
+
     }
     return dummy.next;
 }
+
+   
