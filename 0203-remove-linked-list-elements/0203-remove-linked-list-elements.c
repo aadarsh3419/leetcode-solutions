dc698d34b6909964914fe37958cdf9fd1ce948c5
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-03-15 08:23:34 +0530

    Time: 0 ms (100%), Space: 13.3 MB (16.7%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.c
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.c
@@ -7,16 +7,19 @@
  */
 struct ListNode* removeElements(struct ListNode* head, int val) {
     struct ListNode dummy;
-    dummy.next = head;
+    dummy.next  = head;
     struct ListNode* current = &dummy;
 
-    
     while(current->next!=NULL){
-        
-        if(current->next->val==val)
+        if(current->next->val == val){
             current->next = current->next->next;
-        else 
+        }
+        else{
             current = current->next;
+        }
+
+    
+
     }
     return dummy.next;
 }
