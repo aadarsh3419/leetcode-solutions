6552496a1dbabd4a1e69c6f47eb3e66798cc272e
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-03-20 11:39:20 +0530

    Time: 1 ms (72.21%), Space: 14.6 MB (30.75%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.c
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* merge(struct ListNode* l1,struct ListNode* l2){
+    if(l1==NULL)return l2;
+    if(l2==NULL)return l1;
+    if(l1->val < l2->val){
+        l1->next = merge(l1->next,l2);
+        return l1;
+    }
+    else{
+        l2->next = merge(l2->next,l1);
+        return l2;
+    }
+
+    
+
+}
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) {
+    if(listsSize == 0)return NULL;
+    
+    while(listsSize>1){
+        int i =0, j = (listsSize-1);
+
+
+        while(i<j){
+        lists[i] = merge(lists[i],lists[j]);
+        i++;
+        j--;
+
+    }
+
+    listsSize = (listsSize + 1)/2;
+
+    }
+    return lists[0];
+    
+}
