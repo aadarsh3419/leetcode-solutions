373f7859779abfe70d80f00e6bbff25f46977f69
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-04-11 00:16:31 +0530

    Time: 3 ms (76.19%), Space: 26.5 MB (51.54%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,16 +4,16 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        
         k = k%n
-        def reverse(l,r):
-            while l<r:
-                nums[l],nums[r] = nums[r],nums[l]
-                l+=1
-                r-=1
-        reverse(0,n-1)
-        reverse(0,k-1)
-        reverse(k,n-1)
-        
+        a = n-k
+        b = []
+        for i in range(a,n):
+            b.append(nums[i])
+        for i in range(0,a):
+            b.append(nums[i])
+        for i in range(n):
+            nums[i] = b[i]
+        return nums
+
 
         
