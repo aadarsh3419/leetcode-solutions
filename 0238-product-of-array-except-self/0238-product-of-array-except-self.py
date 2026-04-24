c795a5852e9ff96fc7b9b1ab882e4fe849ada761
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-04-24 20:14:18 +0530

    Time: 11 ms (99.36%), Space: 25.6 MB (55.88%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,16 +1,14 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        ans=[1]*n
-        right=1
-        for i in range(n-1,-1,-1):
-            ans[i]=right
-            right*=nums[i]
-
-        left=1
-        for i in range(n):
-            ans[i]*=left
-            left*=nums[i]
-        return ans
-
+       n = len(nums)
+       ans = [1]*n
+       prefix = 1
+       for i in  range(n):
+        ans[i] = prefix
+        prefix*=nums[i]
+       suffix = 1
+       for i in range(n-1,-1,-1):
+        ans[i]*=suffix
+        suffix*=nums[i]
+       return ans
 
