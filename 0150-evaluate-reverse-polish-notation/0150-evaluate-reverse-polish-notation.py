778d5ed638d8ff9037ca5806a9b462d5188b581c
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-06-07 17:54:33 +0530

    Time: 6 ms (11.55%), Space: 20.6 MB (73.11%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,17 +1,16 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        dicte = {"*":lambda a,b:a*b,
-                 "+":lambda a,b:a+b,
-                 "-":lambda a,b:a-b,
-                 "/":lambda a,b:int(a/b)
-        }
+        dict = {"*":lambda a,b:a*b,
+                "-":lambda a,b:a-b,
+                "+":lambda a,b:a+b,
+                "/":lambda a,b:int(a/b)}
         for i in tokens:
-            if i in dicte:
+            if i not in dict:
+                stack.append(int(i))
+            else:
                 a = stack.pop()
                 b = stack.pop()
-                result = dicte[i](b,a)
+                result = dict[i](b,a)
                 stack.append(result)
-            else:
-                stack.append(int(i))
-        return stack[0]
+        return stack[-1]
