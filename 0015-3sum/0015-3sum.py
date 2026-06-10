27e5a57ef46d6f0130d70659925a6ee26784120c
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-06-10 13:15:25 +0530

    Time: 715 ms (28.22%), Space: 22.2 MB (54.43%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,28 +1,31 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        target = 0
-        p = []
         nums.sort()
-        for i ,v in enumerate(nums):
-            if i>0:
-                if nums[i] == nums[i-1]:
-                    continue
-            j = i+1
-            k =len(nums)-1
-            while j<k:
-                s = nums[i]+nums[j]+nums[k]
-                if s == target:
-                    p.append([nums[i],nums[j],nums[k]])
-                    while j <k and nums[j] == nums[j+1]:
-                        j+=1
-                    while j < k and nums[k] == nums[k-1]:
-                        k-=1
-                    j+=1
-                    k-=1
-                elif s < 0:
-                    j+=1
-                else:
-                    k-=1
-        return p
-                
+        n = len(nums)
+        result = []
 
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            j = i+1
+            k = n-1
+
+            while j < k:
+                    
+                su = nums[i] + nums[j] + nums[k]
+                if su == 0:
+                    result.append([nums[i],nums[j],nums[k]])
+                    k-=1
+                    j+=1
+                    while j<k and nums[j] == nums[j-1]:
+                        j+=1
+                    while j < k and nums[k] == nums[k+1]:
+                        k-=1
+                if su > 0:
+                    k-=1
+                if su < 0:
+                    j+=1
+        return result
+
+
+ 
