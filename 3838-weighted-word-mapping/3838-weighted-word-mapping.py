7ba30aabca72233e7fb6af7419da372687110146
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-06-13 14:38:31 +0530

    Time: 12 ms (25.27%), Space: 19.3 MB (33.64%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.py
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.py
@@ -1,17 +1,15 @@
 class Solution:
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
-        graph = {}
-        for i,w in enumerate(weights):
-            graph[chr(ord("a")+i)] = w
         result = ""
         for word in words:
             score = 0
             for ch in word:
-                score+=graph[ch]
-            idx = score%26
-            result += chr(ord("z")-idx)
+                score+=weights[ord(ch)-ord('a')]
+            result += chr(ord('z')-score%26)
         return result
+
        
 
 
   
+ 
