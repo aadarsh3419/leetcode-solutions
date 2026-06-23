bde5a37cb026bc661cbdd5e4cf9759cf7f72b7f0
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-06-23 23:21:01 +0530

    Time: 294 ms (21.72%), Space: 35.1 MB (95.51%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
@@ -7,12 +7,13 @@ class Solution:
         count = 0
         while right < n:
             hashmap[nums[right]] = hashmap.get(nums[right],0)+1
+            if hashmap[nums[right]] <= k:
+                count = max(count,right-left+1)
             while hashmap[nums[right]] > k:
                 hashmap[nums[left]]-=1
                 left+=1
             
-            if hashmap[nums[right]] <= k:
-                count = max(count,right-left+1)
+            
                 
                 
             
