86d10af9492ad7c59c6c5b06c3cffb004cf3c1f1
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-06-24 13:15:44 +0530

    Time: 307 ms (5.05%), Space: 26.7 MB (11%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,18 +1,22 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        count = {0:1}
-        prefix = 0
-        ans = 0
-        for num in nums:
-            if num %2 !=0:
-                prefix+=1
-            else:
-                0
-            print(prefix)
-            if prefix - k in count:
-                ans+=count[prefix-k]
-                print(ans)
-            count[prefix] = count.get(prefix,0)+1
-        print(count)
-        return ans
+        return self.atmostk(nums,k) - self.atmostk(nums,k-1)
+    def atmostk(self,nums,k): 
+        hashh = {}
+        n = len(nums)
+        left = 0
+        count = 0
+        odd = 0
+        for right in range(n):
             
+            hashh[nums[right]] = hashh.get(nums[right],0)+1
+            if nums[right]%2!=0:
+                odd+=1
+            
+            while odd > k:
+                hashh[nums[left]]-=1
+                if nums[left]%2!=0:
+                    odd-=1
+                left+=1
+            count += right-left+1
+        return count
