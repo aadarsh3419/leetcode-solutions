829b2afeef2de89c4ef896c342ebbe623309cf43
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-06-29 22:38:59 +0530

    Time: 37 ms (7.2%), Space: 19.3 MB (89.37%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -6,13 +6,12 @@ class Solution:
             if remaining == 0:
                 array.append(subset.copy())
                 return
-            if remaining < 0:
-                return
-            if i  == len(candidates):
-                return
-            #we first append the i in subset then minus it with target then recall it until 1 condtion hit abouve then we pop the last element and add the minus we do before undo the remaning then 
-            subset.append(candidates[i])
             
+            if i  == len(candidates) or remaining < 0:
+                return
+            #we first append the i in subset then minus it with target then recall it until 1 condtion hit abouve then we pop the last element and add the minus we do before undo the remaning then
+
+            subset.append(candidates[i])
             dfs(i+1,subset,remaining-candidates[i])
             subset.pop()
             
@@ -20,6 +19,7 @@ class Solution:
             next_i = i
             while next_i+1 < len(candidates) and candidates[next_i] == candidates[next_i+1]:
                 next_i+=1
+                
             dfs(next_i+1,subset,remaining)
         dfs(0,[],target)
         return array
