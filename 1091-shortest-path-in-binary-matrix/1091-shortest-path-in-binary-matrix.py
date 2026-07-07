bde3c97544ee685ca7dcf364f3a94d5e746a6d61
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-07-07 17:59:28 +0530

    Time: 183 ms (40.95%), Space: 20 MB (52.69%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -0,0 +1,52 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+        queue = deque()
+        if grid[0][0] == 1 or grid[n-1][m-1] == 1:
+            return -1
+        if len(grid) == 1 and len(grid[0]) == 1 and grid[0][0] == 0:
+            return 1
+        distance = 0
+        queue.append((0,0))
+        grid[0][0] = -1
+
+        direction = [
+                (-1,0),
+                (1,0),
+                (0,-1),
+                (0,1),
+                (1,1),
+                (-1,-1),
+                (-1,1),
+                (1,-1)
+        ]
+        def bfs():
+            nonlocal distance
+            nonlocal m,n
+            while queue:
+                size = len(queue)
+                for _ in range(size):
+                    r,c = queue.popleft()
+                    if r == n-1 and c == m-1:
+                        return distance + 1
+                    
+                    for dr,dc in direction:
+                        nr = dr + r
+                        nc = dc + c
+                        if nr < 0 or nr >= len(grid) or nc < 0 or nc >=len(grid [0]):
+                            continue
+                        if grid[nr][nc] == 1 or grid[nr][nc] == -1:
+                            continue
+                        if grid[nr][nc] == 0 :
+                            queue.append((nr,nc))
+                            grid[nr][nc] = -1
+                    
+                distance+=1
+                
+        ans = bfs()
+        if ans == None:
+            return -1
+        else:
+            return ans
+        
