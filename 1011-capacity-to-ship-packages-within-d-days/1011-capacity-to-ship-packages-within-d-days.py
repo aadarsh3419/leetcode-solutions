a551f04b86b5c544bc3d671058ecfe9d5c771275
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-07-25 22:25:11 +0530

    Time: 209 ms (78.41%), Space: 23.8 MB (65.58%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -6,20 +6,13 @@ class Solution:
             mid = (left+right)//2
             day = 1
             prefix = 0
-            i = 0
-            n  = len(weights)
-            while i <= n-1:
-                prefix+=weights[i]
-                if prefix<=mid:
-                    i+=1
-                
-                elif prefix > mid:
+            for i in weights:
+                if prefix + i <= mid:
+                    prefix+=i
+                else:
                     day+=1
-                    prefix = 0 
-            if prefix <= mid and i == n-1:
-                day+=1
-                    
-                
+                    prefix = i
+
             if day<=days:
                 right = mid - 1
             elif day> days:
